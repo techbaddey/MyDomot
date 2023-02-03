35ebad8cf22c4aa99443899be245cfa019be5830
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-02-03 19:25:31 +0100

    I validated the sign up and login pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink, Link, Routes, Route } from "react-router-dom";
 import myDomot from "./myDomotLogo.png";
+import shopping from "./shoppingImg.png";
+
 
 
 
@@ -25,72 +27,57 @@ export const Navigation = () => {
 
 
 
-export const SignupPage = () => {
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+class SignupPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-  const onFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-  const onLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // store values of email and password in localStorage
+    localStorage.setItem('email', this.state.email);
+    localStorage.setItem('password', this.state.password);
+    window.location.href = '/login';
+  }
 
-  const onEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const onPasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-      event.preventDefault();
-  };
-
+  render() {
   return (
-    <>
       <div className="login-signup">
         <img src={myDomot} alt="myDomot" className="logo" />
         <div className="form">
           <p className="p-two">Hi there!</p>
           <p className="p-three">Create a new account</p>
-          <form onSubmit={``}>
+      <form onSubmit={this.handleSubmit}>
             <label>First Name<span>*</span></label>
             <input
               type="text"
               placeholder="First Name"
-              value={firstname}
-              onChange={onFirstNameChange}
               required
             />
             <label>Last Name<span>*</span></label>
             <input
               type="text"
               placeholder="Last Name"
-              value={lastname}
-              onChange={onLastNameChange}
               required
             />
             <label>Email<span>*</span></label>
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={onEmailChange}
-              required
-            />
+            <input type="email" name="email" value={this.state.email} 
+          onChange={this.handleChange} placeholder="Email" required />
+            
             <label>Password<span>*</span></label>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={onPasswordChange}
-              required
-            />
+            <input type="password" name="password" value={this.state.password} 
+          onChange={this.handleChange} placeholder="Password" required />
+           
             <input type="submit" value="SIGN UP" />
           </form>
           <p className="p-four">Have an account already?</p>
@@ -99,27 +86,38 @@ export const SignupPage = () => {
           </NavLink>
         </div>
       </div>
-    </>
   );
-};
+}
+}
 
 
-export const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-  const onEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-  const onPasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // retrieve values of email and password from localStorage
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+    // if the stored email and password match the inputted values, redirect user to homepage
+    if (storedEmail === this.state.email && storedPassword === this.state.password) {
+      window.location.href = '/';
+    }
+  }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
+  render () {
   return (
     <>
       <div className="login-signup">
@@ -127,16 +125,13 @@ export const LoginPage = () => {
         <div className="form">
           <p className="p-two">Hello there! Welcome back</p>
           <p className="p-three">Log in to your account</p>
-          <form onSubmit={``}>
+      <form onSubmit={this.handleSubmit}>
             <label>Email<span>*</span></label>
-            <input type="text" value={email} onChange={onEmailChange} required />
+            <input type="email" name="email" value={this.state.email} 
+          onChange={this.handleChange} required />
             <label>Password<span>*</span></label>
-            <input
-              type="password"
-              value={password}
-              onChange={onPasswordChange}
-              required
-            />
+            <input type="password" name="password" value={this.state.password} 
+          onChange={this.handleChange} required />
             <a href="#" className="forgot">
               Forgot Password?
             </a>
@@ -150,7 +145,8 @@ export const LoginPage = () => {
       </div>
     </>
   );
-};
+}
+}
 
 
 function Path() {
