f51037c5164477fa1c6085b92dc2440c7016a791
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-04-01 14:15:49 +0100

    I fixed a bug

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../components/About.css';
 import Footer from './Footer';
-import { Navigation } from "./Category";
+import { Navigation } from "./Hero";
 
 
 function About() {
